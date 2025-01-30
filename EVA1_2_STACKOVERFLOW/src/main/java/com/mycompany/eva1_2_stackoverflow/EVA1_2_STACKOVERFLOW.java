3607e71549407f171159cd2793923c5fc44fb94e
JavierEsp12 <l23550416@chihuahua2.tecnm.mx>
2025-01-30 17:42:25 -0600

    Add files via upload

---
+++ EVA1_2_STACKOVERFLOW/src/main/java/com/mycompany/eva1_2_stackoverflow/EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_stackoverflow;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_2_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    
+    public static void A(){
+        A();
+    }
+}
