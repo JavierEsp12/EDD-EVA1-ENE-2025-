444a1e139804b64d5bfb797ca54a52a0037b876d
JavierEsp12 <l23550416@chihuahua2.tecnm.mx>
2025-02-25 23:58:29 -0600

    Add files via upload

---
+++ EVA1_17_CUBO/src/main/java/com/mycompany/eva1_17_cubo/EVA1_17_CUBO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_17_cubo;
+
+/**
+ *
+ * @author Javier Alonso Espinoza Palafox
+ */
+public class EVA1_17_CUBO {
+
+    public static void main(String[] args) {
+        int cubo[][][] = new int[5][3][4];
+        for (int i = 0; i < cubo.length; i++){
+            for (int j = 0; j < cubo[i].length; j++){
+                for (int k = 0; k < cubo[i][j].length; k++){
+                    cubo[i][j][k] = (int)(Math.random() * 100);
+                }
+            }
+        }
+        for (int i = 0; i < cubo.length; i++){
+            for (int j = 0; j < cubo[i].length; j++){
+                for (int k = 0; k < cubo[i][j].length; k++){
+                    System.out.print("[" + cubo[i][j][k] + "]");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        }
+    }
+}
